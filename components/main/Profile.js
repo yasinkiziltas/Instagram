16c0bbe5921c1400f3c4f24aeb2f10e56fb0b87a
Yasin Kızıltaş <yasinkiziltas@Yasin-MacBook-Air.local>
2021-04-25 08:04:53 +0300

    Profile Detail

--- components/main/Profile.js
+++ components/main/Profile.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { View, Button, Text } from 'react-native'
+import { View, Button, Text, StyleSheet} from 'react-native'
 import * as firebase from 'firebase'
-import { fetchUser } from '../../redux/actions/index'
 
-export default function Profile() {
+import {connect} from 'react-redux'
+
+function Profile(props) {
+    const {currentUser, posts} = props;
+    console.log(currentUser, posts)
 
     function signOut() {
         try {
@@ -14,16 +17,24 @@ export default function Profile() {
         }
     }
 
-    React.useEffect(() => {
-        fetchUser()
-    }, [])
-
-    const { currentUser } = fetchUser().name
-
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', }}>
-            <Text>Welcome! {currentUser}</Text>
+            <Text>Welcome {currentUser.name}</Text>
             <Button title="Sign Out" onPress={() => signOut()} />
         </View>
     )
 }
+
+const stlyes = StyleSheet.create({
+    container:{
+        flex: 1,
+        marginTop: 40
+    }
+})
+
+const mapStateToProps = (store) => ({
+    currentUser: store.userState.currentUser,
+    posts: store.userState.posts
+})
+
+export default connect(mapStateToProps, null)(Profile);
